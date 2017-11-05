50dab45b58841f550997b0a5e5f1db28503a568a
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2017-11-05 11:13:30 +0000

    all revised. read me updated with what was learnt

---
+++ main.js
@@ -0,0 +1,81 @@
+// var a = 'this is a string';
+// var b = 'this is another string';
+// var array = [a, b];
+//
+// console.log(array);
+
+
+// var combinedString = a + b;
+//
+// console.log(combinedString);
+//
+// console.log(a.indexOf('h'));
+//
+// console.log(a.substr(2,4));
+//
+// console.log(a.includes('string'));
+
+
+// var myNumber = 5;
+// var myDecimal = 5.5;
+// var myExponential = 123e5;
+//
+// myNumber++
+//
+// console.log(myNumber + 3);
+//
+// console.log(myNumber - 3);
+//
+// console.log(myNumber * 3);
+//
+// console.log(myNumber / 5);
+//
+// console.log(isFinite(myNumber));
+//
+// console.log(myNumber.toString());
+
+//
+// var myBoolean = true;
+// myBoolean = false;
+
+// var myArray = [1, 2, 3, 'string1', 'string2', 'string3'];
+
+// console.log(myArray[myArray.length - 1]);
+//
+// var join = myArray + ["one thing", "another thing"];
+// console.log(join);
+//
+// console.log(typeof join);
+
+// myArray.push('Where do I go?');
+
+// console.log(myArray);
+//
+// console.log(myArray.pop());
+
+// console.log(myArray.reverse());
+
+// var object = {
+//   one: 'one is a string',
+//   two: ['is an object'],
+//   three: {
+//     nested: 'this is a nested object'
+//   }
+// }
+//
+// console.log(object);
+
+
+
+var myString = '5';
+var myDecimalString = '2.5';
+var myNumber = 3;
+
+//change string into an integer
+console.log( parseInt(myString) + myNumber);
+
+//change string to decimal (float)
+console.log( parseFloat(myDecimalString) + myNumber);
+
+//change decimal to integer (remove the point)
+console.log( parseInt(myDecimalString) + myNumber);
